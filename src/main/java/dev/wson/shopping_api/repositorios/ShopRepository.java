c4f7024a9e811d906b5c4a1dcb4101fe348348e2
wandersonwson <wandersonwson@hotmail.com>
2024-10-18 21:45:02 -0300

    Repositório

---
+++ src/main/java/dev/wson/shopping_api/repositorios/ShopRepository.java
@@ -0,0 +1,14 @@
+package dev.wson.shopping_api.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import dev.wson.shopping_api.modelos.Shop;
+import java.util.List;
+import java.time.LocalDate;
+
+@Repository
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+    List<Shop> findAllByUsuarioId(String usuarioId);
+    List<Shop> findAllByTotalGreaterThan(float total);
+    List<Shop> findAllByDataGreaterThanEquals(LocalDate data);
+}
